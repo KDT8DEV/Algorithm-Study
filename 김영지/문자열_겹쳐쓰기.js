c5fa30d352f3372481a427012b01001c2a11127f
kyj <7saval@gmail.com>
2025-09-10 18:06:09 +0900

    250910 | 문자열 겹쳐쓰기 | 1문제

---
+++ 김영지/문자열_겹쳐쓰기.js
@@ -0,0 +1,35 @@
+// 문자열 my_string, overwrite_string과 정수 s가 주어집니다. 
+// 문자열 my_string의 인덱스 s부터 overwrite_string의 길이만큼을 
+// 문자열 overwrite_string으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+// 제한사항
+// my_string와 overwrite_string은 숫자와 알파벳으로 이루어져 있습니다.
+// 1 ≤ overwrite_string의 길이 ≤ my_string의 길이 ≤ 1,000
+// 0 ≤ s ≤ my_string의 길이 - overwrite_string의 길이
+
+function solution(my_string, overwrite_string, s) {
+    let answer = '';
+    let ml = my_string.length;
+    let ol = overwrite_string.length;
+    let result1 = my_string.substring(0,s);
+    let result2 = overwrite_string;
+    let result3 = "";
+    
+    if(ml> ol+s){
+        result3 = my_string.substring(ol+s);
+    }
+        
+    answer = result1 + result2 + result3;
+    
+    return answer;
+}
+
+// ======================
+// 다른 사람 풀이
+// 불필요한 변수 x
+// 조건문 x
+// substring (또는 slice)는 범위를 벗어나도 안전하게 빈 문자열을 반환
+function solution(my_string, overwrite_string, s) {
+    return my_string.slice(0, s) + overwrite_string + my_string.slice(s + overwrite_string.length);
+}
+
