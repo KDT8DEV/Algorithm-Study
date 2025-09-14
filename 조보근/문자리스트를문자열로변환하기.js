b29fb9c658207887e301cf95196cb01cca00c60f
P2P-J <olnn712@gmail.com>
2025-09-15 02:23:04 +0900

    팩트리얼 외 3문제 = 총 4문제

---
+++ 조보근/문자리스트를문자열로변환하기.js
@@ -0,0 +1,8 @@
+/* 문자들이 담겨있는 배열 arr가 주어집니다. arr의 원소들을 순서대로 이어붙인 문자열을 return하는 solution함수를 작성해주세요.
+ */
+
+function solution(arr) {
+ 
+   let answer = arr.join(''); //여기서 join이라는 것이 배열의 모든 원소를 하나의 문자열로 합쳐주는 함수라고 하네. 알아두면 좋겟어 이건
+   return answer;
+}
