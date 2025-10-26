7b76b2a376932902b1de7274360fca5c7e1c6ee6
P2P-J <olnn712@gmail.com>
2025-10-27 00:06:37 +0900

    251027 | 뒤집힌문자열 외 3문제 | 4문제

---
+++ 조보근/뒤집힌문자열.js
@@ -0,0 +1,12 @@
+/* 문자열 my_string이 매개변수로 주어집니다. my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요. */
+
+function solution(my_string) {
+    var answer = my_string.split('').reverse().join('');
+    return answer;
+}
+
+
+function solution(my_string) {
+    var answer = [...my_string].reverse().join("");
+    return answer;
+}  // 스프링도 저 ...이 가능하네.
