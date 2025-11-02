696fd9d4e21a9d7879355833bc63584ceaf9fab9
Hayeon Baek <bhy0512@gmail.com>
2025-11-02 23:03:37 +0900

    251102 | 파일명 정렬 | 1문제

---
+++ 백하연/8주차/파일명 정렬.js
@@ -0,0 +1,8 @@
+/* 문제 핵심 ➡️ 안정 정렬(Stable Sort)
+ * 파일명을 HEAD + NUMBER + TAIL로 분리
+ * HEAD로 사전순 정렬 -> HEAD가 같으면 NUMBER의 숫자로 정렬 -> HEAD와 NUMBER가 같을 경우 원력 순서 유지
+ */
+function solution(files) {
+}
+solution(['img12.png', 'img10.png', 'img02.png', 'img1.png', 'IMG01.GIF', 'img2.JPG'])
+solution(['F-5 Freedom Fighter', 'B-50 Superfortress', 'A-10 Thunderbolt II', 'F-14 Tomcat'])
