d7161931552a551c4f9e9723a260354bc531f34e
P2P-J <olnn712@gmail.com>
2026-02-03 10:58:30 +0900

    260202 | 네트워크 외 4문제 | 총 5문제

---
+++ 조보근/최댓값만들기2.js
@@ -0,0 +1,12 @@
+/* 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.-10,000 ≤ numbers의 원소 ≤ 10,000
+2 ≤ numbers 의 길이 ≤ 100 */
+
+function solution(numbers) {
+  numbers.sort((a, b) => a - b);
+
+  const minProduct = numbers[0] * numbers[1];
+
+  const maxProduct = numbers[numbers.length - 1] * numbers[numbers.length - 2];
+
+  return Math.max(minProduct, maxProduct);
+}
